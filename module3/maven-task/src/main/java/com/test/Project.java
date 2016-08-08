c56df46141e3b04d18650e706d193758967d2d86
Evgeniy Krupen <evgeniy_krupen@epam.com>
2016-08-08 15:34:51 +0300

    modify java3

--- module3/maven-task/src/main/java/com/test/Project.java
+++ module3/maven-task/src/main/java/com/test/Project.java
@@ -15,6 +15,16 @@ class Project {
 
 }
 
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
+
+
 //comment
 //comment
 //comment
